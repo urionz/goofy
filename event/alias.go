8261aa90beac02f9f09bf7a1b2d1c9a7434ecdc5
urionz <urionz@urionz.com>
2021-05-26 14:46:59 +0800

    update

--- event/alias.go
+++ event/alias.go
@@ -1,12 +1,25 @@
 package event
 
-import "github.com/gookit/event"
+import (
+	"github.com/gookit/event"
+	"github.com/urionz/goofy/log"
+)
 
 type (
-	Event        = event.Event
-	M            = event.M
-	Listener     = event.Listener
-	ListenerFunc = event.ListenerFunc
-	Listeners    = map[string][]Listener
-	BasicEvent   = event.BasicEvent
+	Event      = event.Event
+	M          = event.M
+	Listener   = event.Listener
+	Listeners  = map[string][]Listener
+	BasicEvent = event.BasicEvent
 )
+
+func ListenerFunc(fn func(event.Event) error) event.ListenerFunc {
+	return func(e event.Event) error {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Error(err)
+			}
+		}()
+		return fn(e)
+	}
+}
